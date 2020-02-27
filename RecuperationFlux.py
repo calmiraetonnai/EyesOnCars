864b5dcd29e86e28ae24307ee06628d5178ba27d
AntoineTxekop <59651609+AntoineTxekop@users.noreply.github.com>
2020-02-27 10:15:06 +0100

    Update RecuperationFlux.py

--- RecuperationFlux.py
+++ RecuperationFlux.py
@@ -2,25 +2,30 @@ import cv2 as cv
 
 print(cv.__version__)
 
-vidcap = cv.VideoCapture(0) # 0 pour la webcam
+# vidcap = cv2.VideoCapture('big_buck_bunny_720p_5mb.mp4')
+vidcap = cv.VideoCapture(2)  # usb
+#vidcap = cv.VideoCapture("http://bs:bs@192.168.43.1:8080/video")  # IP
 if (vidcap.isOpened() == False):
-    print("Error opening video stream or file") #signale si la connexion a échoué
-success, image = vidcap.read() #début de la capture
+    print("Error opening video stream or file")
+success, image = vidcap.read()
+# cv.imwrite("fond.jpg", image)  # save new background as jpg file
+# while (True)
 count = 0
-while vidcap.isOpened() and count < 100:
-    if (count % 10 == 0):
-        cv.imwrite("frame%d.jpg" % count, image)  # save frame as jpg file, with the name frameX.jpg with X=count
+while vidcap.isOpened() and count < 1000:
+    # if (count % 10 == 0):
+    #     cv.imwrite("frame%d.jpg" % count, image)  # save frame as jpg file, with the name frameX.jpg with X=count
     success, image = vidcap.read()
-    if success:
-        cv.imshow('Frame', image) #affiche à l'écran l'image capturée
+    # if success:
+    #     #traiterImage
+    #     cv.imshow('Frame', image)
     if cv.waitKey(25) & 0xFF == ord(' '):
-        cv.imwrite("fond.jpg" , image)  # enregistre le nouveau fond en appuyant sur la touche Espace
+        cv.imwrite("fond.jpg" , image)  # save new background as jpg file
         print('Nouveau fond')
 
     if cv.waitKey(25) & 0xFF == ord('q'):
-        break # arrete le flux lorsqu'on appuie sur la touche Q
+        break
 
     count += 1
 
-vidcap.release() # deconnexion du flux
-cv.destroyAllWindows() #fermeture de toutes les fenêtres
+vidcap.release()
+cv.destroyAllWindows()
